6eb4d1cb89bb46dc42757dd7b0131f19768a5a69
SnakeOwl <30445227+SnakeOwl@users.noreply.github.com>
2018-10-10 14:24:56 +0300

    Окно быстрой покупки
    Форма для заполнения полей для быстрой покупки.

---
+++ speedBuy.php
@@ -0,0 +1,19 @@
+<div class="back">
+    <div class="speedBuy">
+        <span>
+    Позвоните в наш офис по телефону указанному ниже и наш оператор оформит ваш заказ.
+    Это нужно для того чтобы обсудить способ оплаты и доставки товара.
+  </span>
+        <br>
+        <span>+375 29 456-25-85</span>
+        <br>
+        <button class="bttn-bordered bttn-lg bttn-primary" id="exit">закрыть окно</button>
+    </div>
+</div>
+
+<script>
+    $('#exit').on('click', function() {
+        $('.back').css('display', 'none')
+    })
+
+</script>
