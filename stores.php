ff3c564939738abecced8ba45faad06ddc38b8bb
SnakeOwl <30445227+SnakeOwl@users.noreply.github.com>
2018-10-10 14:25:42 +0300

    Create stores.php

---
+++ stores.php
@@ -0,0 +1,81 @@
+<?php
+include_once 'head.php';
+include_once 'header.php';
+?>
+    <main>
+        <?php
+    if (isset($_GET['edit'])) {
+        if ($_COOKIE['ID'] != 11) {
+            unLog();
+        } else {
+            $editStode = getStoreByAddr($_GET['edit']);
+            $store = $editStode->fetch_array(MYSQLI_ASSOC);
+            ?>
+            <form action="event.php" method="post" class="panel">
+                <p>изменение магазина</p>
+                <input type="hidden" name="firstaddr" value="<?=$store['Addres']?>">
+                <input type="text" name='nameStore' value="<?=$store['Name']?>" placeholder="наименование магазина"><br>
+                <input type="text" name="addr" value="<?=$store['Addres']?>" placeholder="адресс магазина"><br>
+                <input type="text" name="grafik" value="<?=$store['Grafik']?>" placeholder="график работы" title="указывать форматом: '8.00 - 20.00'"><br>
+                <input type="submit" name="editMagasine" value="Принять">
+            </form>
+            <?php
+        }
+    }
+    ?>
+                <h2>Наши магазины.</h2>
+                <div class="store">
+                    <table>
+                        <tr>
+                            <th>Название</th>
+                            <th>Адрес</th>
+                            <th>График работы</th>
+                        </tr>
+                        <?php
+        $stores = getStores();
+        while ($data = $stores->fetch_array(MYSQLI_ASSOC)) {
+            ?>
+
+
+                            <tr>
+                                <td>
+                                    <?= $data['Name'] ?>
+                                </td>
+                                <td>
+                                    <?= $data['Addres'] ?>
+                                </td>
+                                <td>
+                                    <?= $data['Grafik'] ?>
+                                </td>
+                                <?php
+                if (isset($_COOKIE['ID'])) {
+                            if ($_COOKIE['ID'] == 11){
+                        $peoples = getPeopleByID($_COOKIE['ID']); 
+                                $admin = $peoples->fetch_array(MYSQLI_ASSOC);
+                                if ($_COOKIE['password'] == $admin['Password']){ 
+                                ?>
+                                    <td>
+                                        <form action="event.php" method="post">
+                                            <input type="hidden" name="addr" value="<?= $data['Addres'] ?>">
+                                            <input type="submit" class="bttn-minimal bttn-sm bttn-success" name="delStore" value="удалить">
+                                            <input type="submit" class="bttn-minimal bttn-sm bttn-success" name="editStore" value="изменить">
+                                        </form>
+                                    </td>
+                                    <?php }}} ?>
+
+                            </tr>
+                            <?php } ?>
+                    </table>
+                </div>
+
+
+    </main>
+
+
+
+    <?php
+include_once 'footer.html';
+
+echo '</body></html>';
+$conn->close();
+?>
