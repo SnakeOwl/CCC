1135c2fbc9489786084fb16a4d4dbc27a454daca
SnakeOwl <30445227+SnakeOwl@users.noreply.github.com>
2018-10-10 11:50:53 +0300

    Create administration.php

---
+++ administration.php
@@ -0,0 +1,13 @@
+<?php include_once 'head.php';include_once 'header.php';?>
+<main>
+    <div>
+      <span>Для связи с администрацией можно использовать следующие данные:</span> <br>
+      <span>Email: kolyania1@gmail.com</span> <br>
+      <span>vk.com: <a href="http://vk.com">Антоша</a></span> <br>
+      <span>Facebook: <a href="https://www.facebook.com">facebook</a></span> <br>
+      <span>Телефон (Velcom): +375 29 123 45 56</span> <br>
+      <span>Наши офисы:</span>  <br>
+      <iframe src="https://www.google.com/maps/d/embed?mid=1Uve0q09KcQB6neUB7nXEteaU1acnNUHq" width="960" height="600"></iframe>
+    </div>
+</main>
+<?phpinclude_once 'footer.html';echo '</body></html>';?>
