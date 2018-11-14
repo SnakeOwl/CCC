3c7283aca1c07ef1c5dde764593489c661fdb020
SnakeOwl <30445227+SnakeOwl@users.noreply.github.com>
2018-11-14 21:42:27 +0300

    Update head.php

--- head.php
+++ head.php
@@ -16,16 +16,23 @@ if (isset($_COOKIE['ID']) & isset($_COOKIE['password'])) {
 }
 ?>
 
-    <!DOCTYPE html>
-    <html lang="ru">
+<!DOCTYPE html>
+<html lang="ru">
 
-    <head>
-        <title>Quick mouses</title>
-        <meta charset="UTF-8">
-        <script src="./Jquery.js"></script>
-        <link rel="SHORTCUT ICON" href="./Images/mouse.ico" type="image/x-icon">
-        <link rel="stylesheet" type="text/css" href="engine1/style.css" />
-
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" href="buttons.css">
-    </head>
+<head>
+    <title>Quick mouses</title>
+    <meta charset="UTF-8">
+    <link rel="SHORTCUT ICON" href="./Images/mouse.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="buttons.css">
+    
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-
+    MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous" defer></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous" defer></script>
+    
+    
+</head>
