3224a0b2987511a99b12425b17affcfa400561bb
SnakeOwl <30445227+SnakeOwl@users.noreply.github.com>
2018-11-14 21:38:00 +0300

    Update administration.php

--- administration.php
+++ administration.php
@@ -1,13 +1,17 @@
 <?php include_once 'head.php';include_once 'header.php';?>
-<main>
+<main class="container">
     <div>
-      <span>Для связи с администрацией можно использовать следующие данные:</span> <br>
-      <span>Email: kolyania1@gmail.com</span> <br>
-      <span>vk.com: <a href="http://vk.com">Антоша</a></span> <br>
-      <span>Facebook: <a href="https://www.facebook.com">facebook</a></span> <br>
-      <span>Телефон (Velcom): +375 29 123 45 56</span> <br>
-      <span>Наши офисы:</span>  <br>
-      <iframe src="https://www.google.com/maps/d/embed?mid=1Uve0q09KcQB6neUB7nXEteaU1acnNUHq" width="960" height="600"></iframe>
+        <span>Для связи с администрацией можно использовать следующие данные:</span>
+        <br>
+        <span>Email: kolyania1@gmail.com</span>
+        <br>
+        <span>vk.com: <a href="http://vk.com">Антоша</a></span>
+        <br>
+        <span>Facebook: <a href="https://www.facebook.com">facebook</a></span>
+        <br>
+        <span>Телефон (Velcom): +375 29 123 45 56</span> <br> <span>Наши офисы:</span>
+        <br>
+        <iframe src="https://www.google.com/maps/d/embed?mid=1Uve0q09KcQB6neUB7nXEteaU1acnNUHq" width="960" height="600"></iframe>
     </div>
 </main>
 <?phpinclude_once 'footer.html';echo '</body></html>';?>
